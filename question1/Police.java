66fe1cad4b7531cd7491656d03c585bbd3b56a53
DHJZYNB <2777482319@qq.com>
2025-03-25 20:21:16 +0800

    project test

---
+++ question1/Police.java
@@ -0,0 +1,16 @@
+package question1;
+
+public class Police {
+	private int ID;
+	private String name;
+	public Police(String name,int ID) {
+		this.name=name;
+		this.ID=ID;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getID() {
+		return ID;
+	}
+}

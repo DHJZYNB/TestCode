bd3cf02bb45b1cc59a2097e7d0eee9b6af42312f
DHJZYNB <2777482319@qq.com>
2025-04-21 17:45:03 +0800

    Subbmision2

--- Test.java
+++ Test.java
@@ -1,6 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
 import java.util.*;
-public Class Test{
-	static void main(String[] args){
-		System.out.println("Hello World!");
-	}
+class Test{
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable(){
+            public void run(){
+                MyFrame frame = new MyFrame();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
+
+class MyFrame extends JFrame{
+    public MyFrame(){
+        this.setSize(400,300);
+        this.setTitle("Test for Java GUI!");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
